2871e811bc2240350712dc0660819cec7508fb0e
Skeith <skeith@asterialabs.io>
2022-05-02 05:26:59 +0800

    Add basic mint page and head metadata propagation support

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+const route = useRoute();
+
+useHead({
+  title: `Magic Token - ${route.meta.title}`,
+  meta: [
+    { name: 'description', content: 'Magic Token Mint' },
+    { name: 'keywords', content: 'magic, token, nft, mint, erc721, ethereum' }
+  ]
+})
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<style lang="stylus">
+body
+  background #222
+  color white
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+definePageMeta({ title: 'Mint' })
+
+function onMint() {
+  console.debug('Minting...');
+}
+</script>
+
+<template>
+  <section>
+
+    <header class="fixed top-0 w-screen text-center p-10">
+      <span class="text-2xl">
+        Magic Token Mint
+      </span>
+    </header>
+
+    <footer class="fixed bottom-0 w-screen text-center p-10">
+      <button class="rounded-full bg-yellow-500 p-5 px-10 text-xl text-black" @click="onMint">
+        Mint Now!
+      </button>
+    </footer>
+
+  </section>
+</template>
+
+<style scoped lang="stylus">
+
+</style>
