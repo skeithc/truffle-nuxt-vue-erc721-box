720e5b326754c640764621cbc4d0fbb192d19081
Skeith <skeith@asterialabs.io>
2022-05-08 05:31:42 +0800

    Add network name to config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,6 @@
 export default {
   ETHEREUM: {
-    NETWORK_ID: process.env.ETHEREUM_NETWORK_ID || '666',
+    NETWORK_ID: process?.env?.ETHEREUM_NETWORK_ID || '666',
+    NETWORK_NAME: process?.env?.ETHEREUM_NETWORK_NAME || 'unknown',
   }
 }
